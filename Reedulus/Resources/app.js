c58fcc1582f6e59636eaee1542b35cc546b77723
Vadim Tyukov <brainreflex@gmail.com>
2011-02-15 20:43:32 +0200

    Play with searchBar control

--- Reedulus/Resources/app.js
+++ Reedulus/Resources/app.js
@@ -1,64 +1,19 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
 
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
+var searchBar = Titanium.UI.createSearchBar({ hintText:'input your question', height: 60, showCancel:true });
 
-
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
+searchBar.addEventListener('return', function(e)
+{
+    if( e.value.substr(-1, 1) == '?' ) {
+        Titanium.UI.createAlertDialog({title:'Answer', message:'Yep, yep, yep!' }).show();
+	    searchBar.blur();
+    } else {
+        Titanium.UI.createAlertDialog({title:'Wrong question', message:'Ouch. You gave wrong question.' }).show();
+	    searchBar.blur();
+    }
 });
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
+var window = Titanium.UI.createWindow({ backgroundColor:'#999' });
 
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
-
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
+window.add(searchBar);
+window.open();
